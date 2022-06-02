ecff297944befd964822ebe85a045992576182e4
BenKurrek <bkurrek@uwaterloo.ca>
2022-06-02 13:58:51 -0400

    removed logic

--- src/index.js
+++ src/index.js
@@ -1,59 +1,17 @@
 import {NearContract, NearBindgen, call, view, near} from 'near-sdk-js'
 
 @NearBindgen
-class CoinFlip extends NearContract {
+class RandomHub extends NearContract {
     constructor() {
         //execute the NEAR Contract's constructor
         super()
-        //set default values for the points
-        this.points = {}
     }
 
     /*
-        Flip a coin. Pass in the side (heads or tails) and a random number will be chosen
-        indicating whether the flip was heads or tails. If you got it right, you get a point.
+        Generate a random number within the range of min and max (both inclusive)
     */
     @call
-    flipCoin(side) {
-        // Get the current player and ensure they're in the game state
-        let player = near.predecessorAccountId();
-
-        if(!(player in this.points)) {
-            this.points[player] = 0;
-        }
-
-        env.log(`${player} chose ${side}`);
-
-        // CCC
-        let random = near.randomSeed();
-        env.log(`value was ${random.get(0)}`);
-        env.log(`value2 was ${random.get(0)[0]}`);
-
-        // Get the result of the coin flip
-        let result; 
-        let num = Math.floor(Math.random() * 2);
-        env.log(`Num was ${num}`);
-        let even = num == 0;
-        env.log(`Even was ${even}`);
-        if (even) {
-            result = "heads";
-        } else {
-            result = "tails";
-        }
-
-        // Check if the result was what the player passed in
-        if(result == side) {
-            env.log(`You Get a Point! The result was ${result}`);
-            this.points[player] += 1;
-        } else {
-            env.log(`You lost a point... The result was ${result}`);
-            this.points[player] -= 1;
-        }
-    }
-
-    // View how many points a specific player has
-    @view
-    viewPoints(player) {
-        return this.points[player];
+    generateRandomNumber(min, max) {
+        return 0
     }
 }
