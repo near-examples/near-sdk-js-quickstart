aa6c4f37cab5814fe116436090fd790eed5121e4
BenKurrek <bkurrek@uwaterloo.ca>
2022-06-02 14:00:29 -0400

    testing out cross contract calls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import {NearContract, NearBindgen, call, view, near} from 'near-sdk-js'
-import { Random } from "random-js";
 
 @NearBindgen
 class CoinFlip extends NearContract {
@@ -17,39 +16,8 @@ class CoinFlip extends NearContract {
     @call
     flipCoin(side) {
         // Get the current player and ensure they're in the game state
-        let player = near.predecessorAccountId();
-        if(!(player in this.points)) {
-            this.points[player] = 0;
-        }
-
-        env.log(`${player} chose ${side}`);
-
-        // CCC
-
-        let random = new Random();
-        const value = random.integer(1, 100);
-        env.log(`value was ${value}`);
-
-        // Get the result of the coin flip
-        let result; 
-        let num = Math.floor(Math.random() * 2);
-        env.log(`Num was ${num}`);
-        let even = num == 0;
-        env.log(`Even was ${even}`);
-        if (even) {
-            result = "heads";
-        } else {
-            result = "tails";
-        }
-
-        // Check if the result was what the player passed in
-        if(result == side) {
-            env.log(`You Get a Point! The result was ${result}`);
-            this.points[player] += 1;
-        } else {
-            env.log(`You lost a point... The result was ${result}`);
-            this.points[player] -= 1;
-        }
+        const status = near.jsvmCall('random-hub.examples.near', 'getRandomNumber', [0, 1]);
+        env.log(`status: ${status}`);
     }
 
     // View how many points a specific player has
