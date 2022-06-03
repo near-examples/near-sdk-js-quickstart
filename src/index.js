13f41dbc6693362a4832db7de5f03a54f66e108c
BenKurrek <bkurrek@uwaterloo.ca>
2022-06-03 11:30:02 -0400

    First pass at random generator

--- src/index.js
+++ src/index.js
@@ -10,8 +10,22 @@ class RandomHub extends NearContract {
     /*
         Generate a random number within the range of min and max (both inclusive)
     */
-    @call
-    generateRandomNumber(min, max) {
-        return 0
+    @view
+    generateRandomNumber() {
+        let absoluteMax = 256
+        
+        // Get random string and check what the the character code is at the first index
+        let foo = near.randomSeed(); 
+        // Random between 0 and 65535? I think???
+        let randomNumber = foo.charCodeAt(0);
+        
+        env.log(foo)
+        env.log(randomNumber)
+
+        if(randomNumber > (absoluteMax / 2)) {
+            return 1
+        } else {
+            return 0;
+        }
     }
 }
